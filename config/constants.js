b70e295a02d7e53469ac4a00fc71fde525226bb2
Asiphe <Asiphe.Nyamambi@mixtelematics.com>
2026-08-28 15:11:22 +0200

    config

---
+++ config/constants.js
@@ -0,0 +1,5 @@
+export const TEST_CONFIG = {
+    vus: 1,
+    duration:'1s',
+    sleep: 1,
+};
---
+++ config/url.js
@@ -0,0 +1,4 @@
+export const URLS = {
+    testimonials: 'https://www.ndosiautomation.co.za/APIDEV/testimonials',
+    //secondurl
+};
